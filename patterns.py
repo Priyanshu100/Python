89299a517d942dbaf9707460a7c7f247bbbda1b0
Priyanshu100 <72188581+Priyanshu100@users.noreply.github.com>
2020-10-04 14:58:54 +0530

    new pyramid pattern

--- patterns.py
+++ patterns.py
@@ -35,3 +35,31 @@ for i in range(1,6):
         print('*',end = " ")
 
     print("")
+ 
+# Let's print star pyramid pattern:
+#   *
+#  * *
+# * * *
+#* * * *
+size=int(input("Enter the range:"))
+
+for i in range(size):
+    for j in range((size-i) - 1):
+        print(end=" ")
+    for j in range(i+1):
+        print("*", end=" ")
+    print()    
+    
+ # Now let's print star pyramid pattern in reverse order:
+#* * * *
+# * * *
+#  * *
+#   *
+size = int(input("Enter the range:"))
+
+for i in range(size,0,-1):
+    for j in range(0,size-i):
+        print(end=" ")
+    for j in range(0,i):
+        print("*",end=" ")
+    print()    
